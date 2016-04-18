79f6610723d09c98ab043883492f8d50738ea628
이준수 <ljs93kr@gmail.com>
2016-04-19 01:08:15 +0900

    coil 프로젝트 시작 과 동시에 NavigationView 세팅을 완료하였습니다

---
+++ app/src/main/java/com/brianandroid/myzzung/coli/CoilApplication.java
@@ -0,0 +1,11 @@
+package com.brianandroid.myzzung.coli;
+
+import android.app.Application;
+
+/**
+ * Created by myZZUNG on 2016. 4. 18..
+ */
+public class CoilApplication extends Application {
+
+
+}
