d4a7ed852928c7babf9cd29a5e7bb1bb545c3ba0
이준수 <ljs93kr@gmail.com>
2016-04-30 20:59:55 +0900

    Navigation View 아이템을 청사진에 맞게 만들어놨습니다

--- app/src/main/java/com/brianandroid/myzzung/coli/ui/MainActivity.java
+++ app/src/main/java/com/brianandroid/myzzung/coli/ui/MainActivity.java
@@ -4,11 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.ViewPager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -18,12 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Adapter;
 
 import com.brianandroid.myzzung.coli.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -115,20 +107,18 @@ public class MainActivity extends AppCompatActivity
         Fragment fragment = null;
         String title = getString(R.string.app_name);
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
             fragment = new HomeFragment();
             title = "Homes";
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_mycoupon) {
+
+        } else if (id == R.id.nav_search) {
+
+        } else if (id == R.id.nav_settings) {
             fragment = new SettingFragment();
             title = "Settings";
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        }else if(id == R.id.nav_community){
 
         }
         if (fragment != null) {
