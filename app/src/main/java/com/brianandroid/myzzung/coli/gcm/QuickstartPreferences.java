c9a2430afcb8999e2b3de9f7c6f9cd690b33d8c2
이준수 <ljs93kr@gmail.com>
2016-05-06 22:28:29 +0900

    gcm 메세지 연동 다시 성공했습니다.

---
+++ app/src/main/java/com/brianandroid/myzzung/coli/gcm/QuickstartPreferences.java
@@ -0,0 +1,10 @@
+package com.brianandroid.myzzung.coli.gcm;
+
+/**
+ * Created by myZZUNG on 2016. 5. 6..
+ */
+public class QuickstartPreferences {
+    public static final String REGISTRATION_READY = "registrationReady";
+    public static final String REGISTRATION_GENERATING = "registrationGenerating";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+}
--- app/src/main/java/com/brianandroid/myzzung/coli/ui/SettingFragment.java
+++ app/src/main/java/com/brianandroid/myzzung/coli/ui/SettingFragment.java
@@ -1,73 +1,162 @@
 package com.brianandroid.myzzung.coli.ui;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.brianandroid.myzzung.coli.R;
+import com.brianandroid.myzzung.coli.gcm.QuickstartPreferences;
+import com.brianandroid.myzzung.coli.gcm.RegisterationIntentService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link SettingFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
  */
 public class SettingFragment extends Fragment {
 
-    //private OnFragmentInteractionListener mListener;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
+
+    private Button mRegistrationButton;
+    private ProgressBar mRegistrationProgressBar;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private TextView mInformationTextView;
 
     public SettingFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_setting, container, false);
+
+
+        registBroadcastReceiver();
+        setLocalBoradcastManager(getActivity());
+
+        // 토큰을 보여줄 TextView를 정의
+        mInformationTextView = (TextView) rootView.findViewById(R.id.informationTextView);
+        //mInformationTextView.setVisibility(View.GONE);
+        // 토큰을 가져오는 동안 인디케이터를 보여줄 ProgressBar를 정의
+        mRegistrationProgressBar = (ProgressBar) rootView.findViewById(R.id.registrationProgressBar);
+        mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+        // 토큰을 가져오는 Button을 정의
+        mRegistrationButton = (Button) rootView.findViewById(R.id.registrationButton);
+        mRegistrationButton.setOnClickListener(new View.OnClickListener() {
+            /**
+             * 버튼을 클릭하면 토큰을 가져오는 getInstanceIdToken() 메소드를 실행한다.
+             * @param view
+             */
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(), "click", Toast.LENGTH_SHORT).show();
+                getInstanceIdToken();
+            }
+        });
+
+        return rootView;
     }
 
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
+    /**
+     * Google Play Service를 사용할 수 있는 환경이지를 체크한다.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(),
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+            }
+            return false;
+        }
+        return true;
+    }
 
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
+    /**
+     * Instance ID를 이용하여 디바이스 토큰을 가져오는 RegistrationIntentService를 실행한다.
+     */
+    public void getInstanceIdToken() {
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(getActivity(), RegisterationIntentService.class);
+            getActivity().startService(intent);
+        }else{
+            Toast.makeText(getActivity(), "no in regis service", Toast.LENGTH_SHORT).show();
+        }
+    }
 
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
+    /**
+     * LocalBoardcastManager 를 등록한다
+     * @param context
+     */
+    private void setLocalBoradcastManager(Context context){
+        LocalBroadcastManager.getInstance(context).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_READY));
+        LocalBroadcastManager.getInstance(context).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_GENERATING));
+        LocalBroadcastManager.getInstance(context).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+    }
 
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p/>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
+    /**
+     * LocalBoardcastManager를 해제한다
+     * @param context
+     */
+    private void deleteLocalBoardcastManager(Context context){
+        LocalBroadcastManager.getInstance(context).unregisterReceiver(mRegistrationBroadcastReceiver);
+    }
+
+    /**
+     * LocalBroadcast 리시버를 정의한다. 토큰을 획득하기 위한 READY, GENERATING, COMPLETE 액션에 따라 UI에 변화를 준다.
+     */
+    public void registBroadcastReceiver(){
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+
+
+                if(action.equals(QuickstartPreferences.REGISTRATION_READY)){
+                    // 액션이 READY일 경우
+                    mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+                    mInformationTextView.setVisibility(View.GONE);
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_GENERATING)){
+                    // 액션이 GENERATING일 경우
+                    mRegistrationProgressBar.setVisibility(ProgressBar.VISIBLE);
+                    mInformationTextView.setVisibility(View.VISIBLE);
+                    mInformationTextView.setText("gcm token 생성중...");
+                } else if(action.equals(QuickstartPreferences.REGISTRATION_COMPLETE)){
+                    // 액션이 COMPLETE일 경우
+                    mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+                    mRegistrationButton.setText("gcm token 생성완료!");
+                    mRegistrationButton.setEnabled(false);
+                    String token = intent.getStringExtra("token");
+                    mInformationTextView.setText(token);
+                }
+
+            }
+        };
+    }
+
+
 }
