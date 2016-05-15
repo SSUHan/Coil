60d5bfc7a8db7cb58de85ab0a683561aace98dff
이준수 <ljs93kr@gmail.com>
2016-05-15 17:53:06 +0900

    settingFragment 에서 gcm 관련 코드를 삭제하고, 서비스 준비중 화면을 넣었습니다

--- app/src/main/java/com/brianandroid/myzzung/coli/ui/SettingFragment.java
+++ app/src/main/java/com/brianandroid/myzzung/coli/ui/SettingFragment.java
@@ -30,13 +30,10 @@ import com.google.android.gms.common.GooglePlayServicesUtil;
  */
 public class SettingFragment extends Fragment {
 
-    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
-    private static final String TAG = "MainActivity";
 
-    private Button mRegistrationButton;
-    private ProgressBar mRegistrationProgressBar;
-    private BroadcastReceiver mRegistrationBroadcastReceiver;
-    private TextView mInformationTextView;
+    private static final String TAG = "SettingFragment";
+
+
 
     public SettingFragment() {
         // Required empty public constructor
@@ -49,114 +46,11 @@ public class SettingFragment extends Fragment {
         ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_setting, container, false);
 
 
-        registBroadcastReceiver();
-        setLocalBoradcastManager(getActivity());
-
-        // 토큰을 보여줄 TextView를 정의
-        mInformationTextView = (TextView) rootView.findViewById(R.id.informationTextView);
-        //mInformationTextView.setVisibility(View.GONE);
-        // 토큰을 가져오는 동안 인디케이터를 보여줄 ProgressBar를 정의
-        mRegistrationProgressBar = (ProgressBar) rootView.findViewById(R.id.registrationProgressBar);
-        mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
-        // 토큰을 가져오는 Button을 정의
-        mRegistrationButton = (Button) rootView.findViewById(R.id.registrationButton);
-        mRegistrationButton.setOnClickListener(new View.OnClickListener() {
-            /**
-             * 버튼을 클릭하면 토큰을 가져오는 getInstanceIdToken() 메소드를 실행한다.
-             * @param view
-             */
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "click", Toast.LENGTH_SHORT).show();
-                getInstanceIdToken();
-            }
-        });
 
         return rootView;
     }
 
-    /**
-     * Google Play Service를 사용할 수 있는 환경이지를 체크한다.
-     */
-    private boolean checkPlayServices() {
-        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
-        if (resultCode != ConnectionResult.SUCCESS) {
-            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
-                GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(),
-                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
-            } else {
-                Log.i(TAG, "This device is not supported.");
-            }
-            return false;
-        }
-        return true;
-    }
 
-    /**
-     * Instance ID를 이용하여 디바이스 토큰을 가져오는 RegistrationIntentService를 실행한다.
-     */
-    public void getInstanceIdToken() {
-        if (checkPlayServices()) {
-            // Start IntentService to register this application with GCM.
-            Intent intent = new Intent(getActivity(), RegisterationIntentService.class);
-            getActivity().startService(intent);
-        }else{
-            Toast.makeText(getActivity(), "no in regis service", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    /**
-     * LocalBoardcastManager 를 등록한다
-     * @param context
-     */
-    private void setLocalBoradcastManager(Context context){
-        LocalBroadcastManager.getInstance(context).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_READY));
-        LocalBroadcastManager.getInstance(context).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_GENERATING));
-        LocalBroadcastManager.getInstance(context).registerReceiver(mRegistrationBroadcastReceiver,
-                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
-    }
-
-    /**
-     * LocalBoardcastManager를 해제한다
-     * @param context
-     */
-    private void deleteLocalBoardcastManager(Context context){
-        LocalBroadcastManager.getInstance(context).unregisterReceiver(mRegistrationBroadcastReceiver);
-    }
-
-    /**
-     * LocalBroadcast 리시버를 정의한다. 토큰을 획득하기 위한 READY, GENERATING, COMPLETE 액션에 따라 UI에 변화를 준다.
-     */
-    public void registBroadcastReceiver(){
-        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-
-
-                if(action.equals(QuickstartPreferences.REGISTRATION_READY)){
-                    // 액션이 READY일 경우
-                    mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
-                    mInformationTextView.setVisibility(View.GONE);
-                } else if(action.equals(QuickstartPreferences.REGISTRATION_GENERATING)){
-                    // 액션이 GENERATING일 경우
-                    mRegistrationProgressBar.setVisibility(ProgressBar.VISIBLE);
-                    mInformationTextView.setVisibility(View.VISIBLE);
-                    mInformationTextView.setText("gcm token 생성중...");
-                } else if(action.equals(QuickstartPreferences.REGISTRATION_COMPLETE)){
-                    // 액션이 COMPLETE일 경우
-                    mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
-                    mRegistrationButton.setText("gcm token 생성완료!");
-                    mRegistrationButton.setEnabled(false);
-                    String token = intent.getStringExtra("token");
-                    mInformationTextView.setText(token);
-                }
-
-            }
-        };
-    }
 
 
 }
